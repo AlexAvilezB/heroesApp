372ffca2fdb3c06dc227ee28bcb5f96cf5d988a4
Alejandro Avilez <aleantonio656@gmail.com>
2022-12-17 02:10:29 -0600

    informacion del heroe mediante id, variables de entorno, switchMap

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../../interface/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( private activatedRoute: ActivatedRoute, private heroesService: HeroesService, private router: Router ) {
   }
 
+  heroe!:Heroe;
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.pipe(
+      switchMap(
+        ({ id }) => this.heroesService.getHeroeById( id )
+      )
+    ).subscribe( resp => this.heroe = resp)
+
+    // this.activatedRoute.params.subscribe( ({ id }) => {
+    //   this.heroesService.getHeroeById(id).subscribe( data => {
+    //     this.heroe = data;
+    //   })
+    // } )
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes/listado'])
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
@@ -12,10 +14,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatProgressSpinnerModule,
+    MatToolbarModule,
   ],
 })
 export class MaterialModule {}
