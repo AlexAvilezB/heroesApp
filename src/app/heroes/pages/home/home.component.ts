1c56f62d951670ef193ddf6cbae1dff3109cbf38
Alejandro Avilez <aleantonio656@gmail.com>
2022-12-20 19:56:23 -0600

    Auth service, guards, login y proteccion de rutas

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [ 
+  styles: [
     `
       .container {
         margin: 10px;
       }
-    `
-  ]
+    `,
+  ],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get auth() {
+    return this.authService.auth;
   }
 
+  constructor( private router: Router, private authService: AuthService ) {}
+
+  logout() {
+    // Ir a Backend
+    //un usuario
+
+    this.router.navigate(['./auth']);
+  }
 }
